6a4620965585c39290a0124b3d85fc7a0034bd90
Anuthida Wuthiphitaksak <anuthida.w64@rsu.ac.th>
2024-11-11 14:09:30 +0700

    .

--- my-app/src/app/Admin/dashboard/page.tsx
+++ my-app/src/app/Admin/dashboard/page.tsx
@@ -4,23 +4,16 @@ import { useEffect, useState } from 'react';
 import NavbarAdmin from '@/components/NavbarAdmin';
 import Image from 'next/image';
 import ChartComponent from '@/components/Requestamount';
-import 'flowbite';
 import { apiFetch } from '@/information/api';
 import WeeklyRequestsChart from '@/components/Typeamount';
 import YearlyRequestsChart from '@/components/Yearamount';
 import IssueTypeamount from '@/components/IssueTypeamount';
 
 export default function Dashbord() {
-  const [isClient, setIsClient] = useState(false);
-
-  // Ensure that the component runs on the client side only
   useEffect(() => {
-    setIsClient(true);  // Mark the component as mounted in the client
+    // โหลด flowbite เฉพาะใน client side
+    import('flowbite');
   }, []);
-
-  if (!isClient) {
-    return null;  // Render nothing until mounted in the client
-  }
 
   return (
     <div className='flex flex-col min-h-screen'>
