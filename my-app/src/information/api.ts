ce153f54d20a9c6fafd9e4c0c8457a68db8414af
Anuthida Wuthiphitaksak <anuthida.w64@rsu.ac.th>
2024-11-11 11:08:35 +0700

    .

--- my-app/src/information/api.ts
+++ my-app/src/information/api.ts
@@ -1,5 +1,5 @@
 // api.ts
-const BASE_URL = 'https://kaizen-request.vercel.app/api';
+const BASE_URL = 'https://kaizen-request.vercel.app';
 
 export const apiFetch = async (endpoint: string, options?: RequestInit): Promise<Response> => {
     return fetch(`${BASE_URL}${endpoint}`, options);
