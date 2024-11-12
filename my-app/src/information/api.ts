6da92f343fb6ffabaf826ab129ae37c13fb85f3c
Anuthida Wuthiphitaksak <anuthida.w64@rsu.ac.th>
2024-11-12 16:22:45 +0700

    .

--- my-app/src/information/api.ts
+++ my-app/src/information/api.ts
@@ -1,5 +1,5 @@
 // api.ts
-const BASE_URL = 'https://p-request-api.vercel.app';
+const BASE_URL = 'https://p-request.onrender.com';
 
 export const apiFetch = async (endpoint: string, options?: RequestInit): Promise<Response> => {
     return fetch(`${BASE_URL}${endpoint}`, options);
