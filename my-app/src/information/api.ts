bab2939de5f014479be2649b3ab6891710dc1855
Anuthida Wuthiphitaksak <anuthida.w64@rsu.ac.th>
2024-11-13 10:43:56 +0700

    .

--- my-app/src/information/api.ts
+++ my-app/src/information/api.ts
@@ -1,5 +1,5 @@
-// api.ts
-const BASE_URL = 'https://p-request.onrender.com';
+// api.ts ในโฟลเดอร์ my-app
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://p-request.onrender.com'; // ค่าเริ่มต้นหากไม่มีตัวแปร
 
 export const apiFetch = async (endpoint: string, options?: RequestInit): Promise<Response> => {
     return fetch(`${BASE_URL}${endpoint}`, options);
