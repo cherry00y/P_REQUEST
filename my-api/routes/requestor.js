fdc3124890b5facb8d3909ffc45052eda90ea6c3
Anuthida Wuthiphitaksak <anuthida.w64@rsu.ac.th>
2024-11-14 15:47:36 +0700

    .

--- my-api/routes/requestor.js
+++ my-api/routes/requestor.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const multer = require('multer');
+const path = require('path');
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.SECRET_KEY; 
 const Requestor = require('../models/requestor');
