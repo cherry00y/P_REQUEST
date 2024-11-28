1ee9aba1975d4fbd0a3c79aa5d0a54a5a6c3ccd7
Anuthida Wuthiphitaksak <anuthida.w64@rsu.ac.th>
2024-11-28 17:09:00 +0700

    .

--- my-app/src/app/Worker/RepairRequest/Implement/page.tsx
+++ my-app/src/app/Worker/RepairRequest/Implement/page.tsx
@@ -313,7 +313,7 @@ export default function Implement() {
                                         className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         min="08:00"
                                         max="18:00"
-                                        value={implementStart || "08:00"}
+                                        value={implementStart || ''}
                                         onChange={(e) => setImplementStart(e.target.value)}
                                         required
                                     />
@@ -334,7 +334,7 @@ export default function Implement() {
                                         className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         min="08:00"
                                         max="18:00"
-                                        value={implementEnd || "18:00"}
+                                        value={implementEnd || ''}
                                         onChange={(e) => setImplementEnd(e.target.value)}
                                         required
                                     />
